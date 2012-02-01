50009758c85639b1430b2736152bf28d7fc3f501
Brent Roman <brent@mbari.org>
2012-02-01 23:04:08 +0000

    use the /sbin/route command rather than /bin/ip

---
+++ esp/ppp/pptp-1.7.2-routing.c
@@ -0,0 +1,107 @@
+/*
+    routing.c, manipulating routing table for PPTP Client
+    Copyright (C) 2006  James Cameron <quozl@us.netrek.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "routing.h"
+
+/*
+
+Design discussion.
+
+The primary task of this module is to add a host route to the PPTP
+server so that the kernel continues to deliver PPTP control and data
+connection packets to the server despite the new PPP interface that is
+created.  The flag --no-host-route is to disable this (not yet implemented).
+
+A secondary task may be to implement all-to-tunnel routing if the
+appropriate flag is specified on the command line.  The flag
+--route-all is to implement this (not yet implemented).
+
+Revised:  brent@mbari.org Feb 2, 2012
+
+The 'ip' command is not available on many memory constrained embedded systems
+(It's about 250kB!)
+This is an alternative implementation using the standard 'route' command instead.
+*/
+
+static char *oldIface = NULL;
+static struct in_addr svrAdr;
+
+void routing_init(char *ip) {
+  static const char *word = " \t\n";
+  char line[256];
+  snprintf(line, sizeof(line), "/sbin/route -n");
+  FILE *p;
+  if (!inet_aton(ip, &svrAdr)) {
+    syslog (LOG_ERR, "routing_init() was passed an invalid IPv4 address");
+    return;
+  }
+  p = popen(line, "r");
+  if (!p) {
+    syslog (LOG_ERR, "Cannot execute route utility: %s", strerror(errno));
+    return;
+  }
+  while (fgets(line, sizeof(line), p)) {
+    char *adrs, *netMasks, *flags, *iface;
+    struct in_addr adr, netMask;
+    if ((adrs = strtok(line, word)) && inet_aton(adrs, &adr) && 
+          strtok(NULL, word) &&   //skip gateway
+        (netMasks= strtok(NULL, word)) && inet_aton(netMasks, &netMask) &&
+        (flags=strtok(NULL, word)) && strchr(flags, 'U') &&
+        strtok(NULL, word) && strtok(NULL, word) && strtok(NULL, word) &&
+        (iface = strtok(NULL, word))) {
+      if ((svrAdr.s_addr & netMask.s_addr) == (adr.s_addr & netMask.s_addr)) {
+        oldIface = strdup(iface);
+        goto ret;
+      }
+    }
+  }
+  syslog(LOG_ERR, "there is no active route to server %s", ip);
+ret:
+  pclose(p);
+}
+
+void routing_start() {
+  if (oldIface) {
+    char buf[300];
+    char *svrIP = inet_ntoa(svrAdr);
+    snprintf(buf, sizeof(buf), "/sbin/route add %s dev %s", svrIP, oldIface);
+    if (system(buf))
+      syslog (LOG_ERR, 
+              "Could not add route to server %s: %s", svrIP, strerror(errno));
+  }
+}
+
+void routing_end() {
+  if (oldIface) {
+    char buf[300];
+    char *svrIP = inet_ntoa(svrAdr);
+    snprintf(buf, sizeof(buf), "/sbin/route del %s dev %s", svrIP, oldIface);
+    if (system(buf))
+      syslog (LOG_ERR,
+              "Could not delete route to %s: %s", svrIP, strerror(errno));
+  }
+}
