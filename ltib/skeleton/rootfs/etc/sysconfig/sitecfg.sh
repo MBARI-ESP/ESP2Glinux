1ab920d1789adfd01740f387a0c221a705a71efa
Brent Roman <brent@mbari.org>
2019-08-18 20:48:00 -0700

    retry tunnel2shore via inittab entry

--- ltib/skeleton/rootfs/etc/sysconfig/sitecfg.sh
+++ ltib/skeleton/rootfs/etc/sysconfig/sitecfg.sh
@@ -7,6 +7,11 @@ ESPshore=134.89.2.91  #ESP shore server
 gatewayUpdated() {
 #invoked after gateway interface updated
 #first argument is previous gateway interface's name
-  gate=`topIf` && [ "$1" != "$gate" ] && ( /usr/sbin/tunnel2shore& )
+  gate=`topIf` && [ "$gate" -a "$1" != "$gate" ] && {
+    tunFn=/var/run/tunnel2shore
+    tunPID=`cat $tunFn`
+    rm $tunFn
+    kill $tunPID  #causes inittab to restart tunnel2shore
+  }
   return 0
 }
