3a8f3e3c58cc724b9db769756629c8c19001db04
Brent Roman <brent@mbari.org>
2021-10-31 10:59:43 -0700

    point to espshore2.mbari.org

--- ltib/skeleton/rootfs/etc/sysconfig/sitecfg.sh
+++ ltib/skeleton/rootfs/etc/sysconfig/sitecfg.sh
@@ -1,7 +1,7 @@
 #Site specific networking utilities & definitions
-# -- revised: 8/31/20 brent@mbari.org
+# -- revised: 10/31/21 brent@mbari.org
 
-ESPshore=134.89.2.91  #ESP shore server
+ESPshore=134.89.2.90  #ESPshore2 server
 
 closeTunnels() {
   #signal tunnel deamons that interface will close soon
